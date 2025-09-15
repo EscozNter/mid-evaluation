d71389ec2eb90e8a747d57e28ee6d705cc298679
Miguel Escoz <miguel.escoz@nter.es>
2025-09-15 11:28:37 +0200

    Add Maven Wrapper and initialize Spring Boot project with essential configuration

---
+++ src/main/java/net/escoz/evaluation/EscozEvaluationApp.java
@@ -0,0 +1,13 @@
+package net.escoz.evaluation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EscozEvaluationApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EscozEvaluationApp.class, args);
+	}
+
+}
